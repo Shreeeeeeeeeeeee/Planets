cf6bdf52851eefbe24e2b22eb44351b81af3cfcb
Shreeeeeeeeeeeee <159097542+Shreeeeeeeeeeeee@users.noreply.github.com>
2024-02-27 08:24:46 +0530

    Update Shreeproject.py

--- Shreeproject.py
+++ Shreeproject.py
@@ -6,7 +6,19 @@ import streamlit as st
 
 st.title('Trajectory of Planets')
 st.header('',divider='rainbow')
-pla=st.radio('Select the planet',("Sun","Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto","Haleys Comet","All"))
+pla1=st.checkbox("Sun")
+pla2=st.checkbox("Mercury")
+pla3=st.checkbox("Venus")
+pla4=st.checkbox("Earth")
+pla5=st.checkbox("Mars")
+pla6=st.checkbox("Jupiter")
+pla7=st.checkbox("Saturn")
+pla8=st.checkbox("Uranus")
+pla9=st.checkbox("Neptune")
+pla10=st.checkbox("Pluto")
+pla11=st.checkbox("Haleys Comet")
+pla12=st.checkbox("All")
+#pla=st.radio('Select the planet',("Sun","Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto","Haleys Comet","All"))
 
 #SUN
 
@@ -486,8 +498,44 @@ def halc():
     plt.savefig('graph.jpg')
     st.image('graph.jpg')
 
+if pla1 :
+     st.image("36dg.gif",caption="The Sun")
+    starsun()
+if pla2:
+    st.image("Mercury.gif",caption="Mercury")
+    merc()
+if pla3:
+     st.image("venus.gif",caption="Venus")
+    ven()
+if pla4:
+    st.image("Earth.gif",caption="Earth")
+    ear()
+if pla5:
+    st.image("Mars.gif",caption="Mars")
+    mar()
+if pla6:
+    st.image("Jupiter.gif",caption="Jupiter")
+    jup()
+if pla7:
+    st.image("Saturn.gif",caption="Saturn")
+    sat()
+if pla8:
+     st.image("Uranus.gif",caption="Uranus")
+    ura()
+if pla9:
+    st.image("Neptune.gif",caption="Neptune")
+    nep()
+if pla10:
+     st.image("Pluto.gif",caption="Pluto")
+    plu()
+if pla11:
+     st.image("Halleys_Comet.gif",caption="Halley's Comet")
+    halc()
+if pla12:
+     st.image("Solar_System.gif",caption="Solar System")
+    all()
 
-if pla=="Sun":
+"""if pla=="Sun":
     st.image("36dg.gif",caption="The Sun")
     starsun()
 elif pla=="Mercury":
@@ -522,6 +570,6 @@ elif pla=="All":
     all()
 elif pla=="Haleys Comet":
     st.image("Halleys_Comet.gif",caption="Halley's Comet")
-    halc()
+    halc()"""
 
 
